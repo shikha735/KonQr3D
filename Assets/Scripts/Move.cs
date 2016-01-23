cf6be5d58be4e8da1a65e8fecfddb83ce74b5507
Shikha Sharma <shikha735@gmail.com>
2016-01-23 17:35:33 +0530

    Player- Jelly Fish Girl imported

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
+public class Move : MonoBehaviour
+{
+    [SerializeField, HideInInspector]
+    NavMeshAgent agent;
+    [SerializeField, HideInInspector]
+    Animator animator;
+
+    void Reset()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
+}
