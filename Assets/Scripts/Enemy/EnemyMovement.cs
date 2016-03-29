bd14c317f7427d32cfffec70e13ccc315caef688
Shikha Sharma <shikha735@gmail.com>
2016-03-29 20:28:48 +0530

    Game Over Animation created
    Enemy animator controllers, Game Over Animation, UI elements, Manager
    Scripts and Enemy Scripts added

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,29 +4,29 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
-    // PlayerHealth playerHealth;
-    // EnemyHealth enemyHealth;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
     NavMeshAgent nav;
 
 
-    void Awake ()
+    void Awake()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-        // playerHealth = player.GetComponent<PlayerHealth>();
-        // enemyHealth = GetComponent<EnemyHealth>();
-        nav = GetComponent <NavMeshAgent> ();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
     }
 
 
-    void Update ()
+    void Update()
     {
-        // if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        // {
-        nav.SetDestination(player.position);
-        // }
-        // else
-        // {
-        // nav.enabled = false;
-        // }
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
     }
 }
