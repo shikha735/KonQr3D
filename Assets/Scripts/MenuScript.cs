61d89f63d719633546a66a721c9cb9d6bc81bed1
Shikha Sharma <shikha735@gmail.com>
2016-03-30 22:13:18 +0530

    UI changed
    Start Scene added, Reduced the scale of objects in Environment,
    Pause/Resume option added- Player has to press "p" key to pause/resume,
    Added image - for that option in UI

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startButton;
+    public Button quitButton;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas> ();
+        startButton = startButton.GetComponent<Button>();
+        quitButton = quitButton.GetComponent<Button>();
+        quitMenu.enabled = false;
+	}
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startButton.enabled = false;
+        quitButton.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startButton.enabled = true;
+        quitButton.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Level 01");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    public bool paused;
+
+	// Use this for initialization
+	void Start () {
+        paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if(!paused)
+        {
+            Time.timeScale = 1;
+        }
+	}
+}
