411cd5b6c8c5ac75d2aef2ad79ab979fb2bb0736
Shikha Sharma <shikha735@gmail.com>
2016-03-31 13:21:30 +0530

    High Score Text added

---
+++ Assets/Scripts/Managers/HighScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScoreManager : MonoBehaviour {
+
+    Text text;
+	
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        text.text = "High Score: " + PlayerPrefs.GetInt("High Score");
+    }
+}
