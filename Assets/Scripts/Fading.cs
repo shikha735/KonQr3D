57459d577b29858439d6f10b5a576906d957a49c
Shikha Sharma <shikha735@gmail.com>
2016-04-10 22:08:23 +0530

    Game Scenes' UI changed and Levels linked

---
+++ Assets/Scripts/Fading.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fading : MonoBehaviour {
+
+    public Texture2D fadeOutTexture; // Texture that will overlay the screen
+    public float fadeSpeed; // fading speed
+
+    private int drawDepth = -1000; // texture's order in heirarchy; Rendered on top
+    private float alpha = 1.0f; // texture's alpha between 0 and 1
+    private int fadeDir = -1; // Direction to fade: in =-1 and out = 1
+
+	void OnGUI()
+    {
+        // Unity's function for rendering GUI
+
+        // fade in/out alpha
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+
+        // clamp the number between 0 and 1 becuase GUI.aplha uses values between 0 and 1
+        alpha = Mathf.Clamp01(alpha);
+
+        // set color of GUI(Canvas); color values remain the same, Alpha value is set to alpha variable
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;                                                          // make black texture render on top
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);   // draw texture to fit entire screen
+    }
+
+    // sets fade direction (fadeDir)
+    public float BeginFade (int direction)
+    {
+        fadeDir = direction;
+        return (3)/*fadeSpeed*/; // return fadeSpeed so that it is easy to time SceneManager.LoadScene
+    }
+
+   void OnLevelWasLoaded()
+    {
+        // alpha = 1; if alpha is not initialized then do it here
+        BeginFade(-1); // call fade in function
+    }
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
+    public float levelStartDelay = 2f;
+    public float levelOneMaxScore = 50;
+    public float levelTwoMaxScore = 100;
+    public float levelThreeMaxScore = 150;
 
-
+    public Text levelText;
+    public int level = 1;
+    public GameObject levelImage;
+    private bool doingSetUp;
     Animator anim;
 
 
@@ -14,6 +23,29 @@ public class GameManager : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    private void OnLevelWasLoaded(int index)
+    {
+        Debug.Log("On level was loaded");
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        Debug.Log("Init Game");
+        doingSetUp = true;
+        // levelImage = GameObject.Find("LevelImage");
+        // levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Level 0" + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+    }
+
+    private void HideLevelImage()
+    {
+        Debug.Log("Hide level image");
+        levelImage.SetActive(false);
+        doingSetUp = false;
+    }
 
     void Update()
     {
@@ -21,19 +53,34 @@ public class GameManager : MonoBehaviour
         {
             anim.SetTrigger("GameOver");
         }
-        if (ScoreManager.score >= 50)
+        if (doingSetUp)
+        {
+            Debug.Log("if doing setup");
+            return;
+        }
+        if (ScoreManager.score >= levelOneMaxScore)
         {
             if (SceneManager.GetActiveScene().name == "Level 01")
             {
+                Debug.Log("Level 2 loading");
+                StartCoroutine(LevelChange());
                 SceneManager.LoadScene("Level 02");
             }
         }
-        if (ScoreManager.score >= 100)
+        if (ScoreManager.score >= levelTwoMaxScore)
         {
             if (SceneManager.GetActiveScene().name == "Level 02")
             {
+                Debug.Log("Level 3 loading");
+                LevelChange();
                 SceneManager.LoadScene("Level 03");
             }
         }
+    }
+
+    IEnumerator LevelChange()
+    {
+        float fadeTime = GameObject.Find("HUDCanvas").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
     }
 }
